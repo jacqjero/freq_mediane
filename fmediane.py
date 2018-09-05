56525985f924983f397a2f2d8a02f52eea155253
jacqjero <jacqjero@gmail.com>
2018-09-05 10:54:25 +0200

    Correction Bug sur la lecture des inputs

--- fmediane.py
+++ fmediane.py
@@ -6,7 +6,7 @@ Created on Fri Jul 13 10:34:09 2018
 """
 
 from recomposition_spectrale import posttrait
-
+import numpy as np
 
 # In[1] Chargement des input
 
@@ -14,10 +14,18 @@ from recomposition_spectrale import posttrait
 with open("Input.txt","r") as fich:
     datain = fich.readlines()
 
-fref = datain[0]
-fe = datain[1]
-fmin_bf = datain[2]
-octbf = datain[3]
+i = 0
+num = np.zeros([np.size(datain),1])
+for st in datain:
+    st1 = np.int(st.split("=")[1].split("\n")[0])
+    num[i] = st1
+    i += 1
+    
+    
+fref = num[0]
+fe = num[1]
+fmin_bf = num[2]
+octbf = num[3]
 
 f = posttrait(fref,fe,fmin_bf,octbf)
 for x in f.Fc:
